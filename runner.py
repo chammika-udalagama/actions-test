0fc971fd558795c4193a99e3d2f9094cf8e56daa
Chammika Udalagama <chammika@nus.edu.sg>
2024-01-11 23:52:23 +0800

    Ready to test the runner.py

--- runner.py
+++ runner.py
@@ -1,4 +1,6 @@
 import subprocess
+import os
+from datetime import datetime
 
 keyword = 'CHATGPT_FEEDBACK'
 
@@ -8,4 +10,15 @@ result = subprocess.run(cmd,
 commit_message = result.stdout
 
 if keyword in commit_message:
-    filename = commit_message.split(' ')[-1]
+    filename = commit_message.split(' ', maxsplit=1)[-1]
+
+    cmd = ['export', f'chatgpt_filename={filename}']
+    subprocess.run(cmd)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    result_filename, _ = os.path.splitext(filename)
+    result_filename = f'{result_filename}_feedback_{timestamp}'
+    result_filename = result_filename.upper() + '.md'
+
+    with open(result_filename, 'w') as file:
+        file.write('HAHAHHA')
