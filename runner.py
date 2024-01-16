45a30ff3e5eeaddc2a2c6b5fe543665c2f83c5a2
Chammika Udalagama <chammika@nus.edu.sg>
2024-01-17 06:36:09 +0800

    Updated runner.py

--- runner.py
+++ runner.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 from datetime import datetime
 
-keyword = 'CHATGPT_FEEDBACK'
+keyword = 'CHATGPT'
 
 cmd = ['git', 'log', '-1', '--pretty=%B']
 result = subprocess.run(cmd,
@@ -15,16 +15,20 @@ result = subprocess.run(cmd,
 commit_message_filename = result.stdout
 
 if keyword in commit_message_for_keyword:
-    filename = commit_message_filename
+    full_path = commit_message_filename
     # commit_message_for_keyword.split(' ', maxsplit=1)[-1]
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+    path_head,filename = os.path.split(full_path)
     result_filename, _ = os.path.splitext(filename)
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     result_filename = f'{result_filename}_feedback_{timestamp}'
     result_filename = result_filename.upper() + '.md'
+    result_path = os.path.join(path_head,result_filename)
 
-    with open(result_filename, 'w') as file:
+    with open(result_path, 'w') as file:
         file.write('HAHAHHA')
 
 
 # print(commit_message_for_keyword)
-# print(commit_message_filename)
+print(result_path)
+print(result_filename)
