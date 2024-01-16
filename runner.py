369c4068ab0326308b57a4a752df0192445f7b78
Chammika Udalagama <chammika@nus.edu.sg>
2024-01-17 06:19:22 +0800

    Modified runner to get last filename

--- runner.py
+++ runner.py
@@ -7,10 +7,16 @@ keyword = 'CHATGPT_FEEDBACK'
 cmd = ['git', 'log', '-1', '--pretty=%B']
 result = subprocess.run(cmd,
                         text=True, shell=False, capture_output=True)
-commit_message = result.stdout
+commit_message_for_keyword = result.stdout
 
-if keyword in commit_message:
-    filename = commit_message.split(' ', maxsplit=1)[-1]
+cmd = ['git', 'log', '-1', '--format=format:', '--name-only']
+result = subprocess.run(cmd,
+                        text=True, shell=False, capture_output=True)
+commit_message_filename = result.stdout
+
+if keyword in commit_message_for_keyword:
+    filename = commit_message_filename
+    # commit_message_for_keyword.split(' ', maxsplit=1)[-1]
     timestamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
     result_filename, _ = os.path.splitext(filename)
     result_filename = f'{result_filename}_feedback_{timestamp}'
@@ -18,3 +24,7 @@ if keyword in commit_message:
 
     with open(result_filename, 'w') as file:
         file.write('HAHAHHA')
+
+
+# print(commit_message_for_keyword)
+# print(commit_message_filename)
